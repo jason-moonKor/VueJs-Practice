ec6dc345fa6d0f4ef24b98def78ebab5f4aeaf9a
jason-moonKor <jaesangv@naver.com>
2021-12-31 18:38:25 +0900

    vue js practicing

---
+++ test/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<h1>Welcome to my {{ title2 }}</h1>
+		<input type="text" v-model="input1" />
+		<button type="button" @click="getData">Get</button>
+	</div>
+</template>
+<script>
+export default {
+	data() {
+		return {
+			title: "JASON VUE JS TEST",
+			title2: "INCHOEN",
+			input1: "abccasdasdd"
+		};
+	},
+	methods: {
+		getData() {
+			alert(this.input1);
+		}
+	}
+};
+</script>
